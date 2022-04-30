c8cf69ce1eb73b5b3dbba4e18582c0e98caf4440
Yekaterina Lamukhina <katerina777lev@gmail.com>
2022-04-30 17:39:18 +0600

    sprint 3

---
+++ src/main/java/Courier.java
@@ -0,0 +1,13 @@
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+/**
+ * Описание курьера.
+ */
+public class Courier {
+    private String login;
+    private String password;
+    private String firstName;
+}
---
+++ src/main/java/OrderClient.java
@@ -0,0 +1,34 @@
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.given;
+
+public class OrderClient extends ScooterRestClient {
+
+    public static final String ORDER_PATH = "api/v1/orders";
+
+    public ValidatableResponse create(Order information) {
+        return given()
+                .spec(getBaseSpec())
+                .body(information)
+                .when()
+                .post(ORDER_PATH)
+                .then();
+    }
+
+    public ValidatableResponse delete(int trackNumber) {
+        return given()
+                .spec(getBaseSpec())
+                .body(trackNumber)
+                .when()
+                .put(ORDER_PATH + "/cancel")
+                .then();
+    }
+
+    public ValidatableResponse orders() {
+        return given()
+                .spec(getBaseSpec())
+                .when()
+                .get(ORDER_PATH)
+                .then();
+    }
+}
